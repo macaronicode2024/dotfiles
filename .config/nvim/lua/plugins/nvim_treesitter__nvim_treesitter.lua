27672486603074208987252574ea54e2aa99b085
acer <macaronicode@gmail.com>
2024-03-17 21:47:12 +0900

    fix: treesitter

--- .config/nvim/lua/plugins/nvim_treesitter__nvim_treesitter.lua
+++ .config/nvim/lua/plugins/nvim_treesitter__nvim_treesitter.lua
@@ -3,13 +3,24 @@ return {
 
   build = ":TSUpdate",
 
-  config = function ()
-    ensure_installed = { 
-      "bash", 
-      "c", "lua", "markdown", "markdown_inline", "typescript", "vim", "vimdoc" }
-    auto_install = true
-    highlight = { enable = true }
-    indent = { enable = true }
-  end
-}
+  opts = {
+    ensure_installed = {
+      "bash",
+      "c",
+      "dart",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "typescript",
+      "vim",
+      "vimdoc",
+    },
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
 
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
+}
