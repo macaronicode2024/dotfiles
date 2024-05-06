a2e19735f686ebceaff52069451de92cfed39eaa
acer <macaronicode@gmail.com>
2024-05-07 06:50:25 +0900

    feat: add daps

---
+++ .config/nvim/lua/plugins/mfussenegger__nvim_dap.lua
@@ -0,0 +1,71 @@
+return {
+  "mfussenegger/nvim-dap",
+
+  dependencies = {},
+
+  config = function()
+    require("dap").adapters["pwa-node"] = {
+      type = "server",
+      host = "localhost",
+      port = "${port}",
+      executable = {
+        command = "node",
+        -- 💀 Make sure to update this path to point to your installation
+        args = { "/home/macaronicode/DapAdapters/js-debug/src/dapDebugServer.js", "${port}" },
+      },
+    }
+
+    require("dap").configurations.javascript = {
+      {
+        type = "pwa-node",
+        request = "launch",
+        name = "Launch file",
+        program = "${file}",
+        cwd = "${workspaceFolder}",
+      },
+    }
+
+    -- https://github.com/mfussenegger/nvim-dap/blob/master/doc/dap.txt#L514
+    vim.keymap.set("n", "<F5>", function()
+      require("dap").continue()
+    end)
+    vim.keymap.set("n", "<F10>", function()
+      require("dap").step_over()
+    end)
+    vim.keymap.set("n", "<F11>", function()
+      require("dap").step_into()
+    end)
+    vim.keymap.set("n", "<F12>", function()
+      require("dap").step_out()
+    end)
+    vim.keymap.set("n", "<Leader>b", function()
+      require("dap").toggle_breakpoint()
+    end)
+    vim.keymap.set("n", "<Leader>B", function()
+      require("dap").set_breakpoint()
+    end)
+    vim.keymap.set("n", "<Leader>lp", function()
+      require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+    end)
+    vim.keymap.set("n", "<Leader>dr", function()
+      require("dap").repl.open()
+    end)
+    vim.keymap.set("n", "<Leader>dl", function()
+      require("dap").run_last()
+    end)
+    vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
+      require("dap.ui.widgets").hover()
+    end)
+    vim.keymap.set({ "n", "v" }, "<Leader>dp", function()
+      require("dap.ui.widgets").preview()
+    end)
+    vim.keymap.set("n", "<Leader>df", function()
+      local widgets = require("dap.ui.widgets")
+      widgets.centered_float(widgets.frames)
+    end)
+    vim.keymap.set("n", "<Leader>ds", function()
+      local widgets = require("dap.ui.widgets")
+      widgets.centered_float(widgets.scopes)
+    end)
+  end,
+}
